f95a561dfd83f0a1ac7e4eb051cf455e72470090
felipesandoli <cfelipesandoli@gmail.com>
2022-12-09 16:29:50 +0100

    Create functions for opening and closing game instructions modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
+const instructionsButton = document.getElementById("instructions-btn");
+const closeModal = document.getElementById("close-btn");
+const modal = document.getElementById("instructions-modal");
 
+// Functions to display and close modal, adapted from w3schools
 
+instructionsButton.onclick = function() {
+    modal.classList.remove("hidden");
+}
+
+closeModal.onclick = function() {
+    modal.classList.add("hidden");
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.classList.add("hidden");
+    }
+}
