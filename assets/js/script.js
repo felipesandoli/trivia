41480118d3becd4824fac25c7ea69b5f7acc0204
felipesandoli <cfelipesandoli@gmail.com>
2022-12-11 17:41:28 +0000

    Add code to function for validating the username

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const instructionsButton = document.getElementById("instructions-btn");
 const closeModal = document.getElementById("close-btn");
 const modal = document.getElementById("instructions-modal");
 
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", validateUsername);
+
 // Functions to display and close modal, adapted from w3schools
 
 instructionsButton.onclick = function() {
@@ -20,10 +23,17 @@ window.onclick = function(event) {
 
 // Function for diplaying the gaming window for playing the game for the first time
 
-function displayGameWindow() {
-    
+function displayGameWindow(username) {
+    console.log(username);
 }
 
 function validateUsername () {
-
+    let username = document.getElementById("username");
+    if (username.value === "") {
+        alert("Please enter a username");
+    } else if (username.value.length < 5){
+        alert("Please choose a username with at least 5 characters");
+    } else {
+        displayGameWindow(username.value);
+    }
 }
